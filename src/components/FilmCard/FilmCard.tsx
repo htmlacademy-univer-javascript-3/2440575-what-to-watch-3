c43f0c85268ce525e586a4e1622145b0cbe9783d
monikahuenika <91833875+monikahuenika@users.noreply.github.com>
2023-11-08 09:07:49 +0500

    add videoplayer

--- src/components/FilmCard/FilmCard.tsx
+++ src/components/FilmCard/FilmCard.tsx
@@ -1,37 +1,76 @@
+import { FilmData } from '../../types/filmData.ts';
 import { Link } from 'react-router-dom';
 import { AppRoute, FilmRoute } from '../../config/config.ts';
-import { FilmData } from '../../types/filmData.ts';
+import VideoPlayer from '../VideoPlayer/VideoPlayer.tsx';
+import { useEffect, useState } from 'react';
 
 type FilmCardProps = {
   film: FilmData;
   clickHandler: (item: FilmData) => void;
-  mouseOverHandler: (item: FilmData) => void;
 }
-const FilmCard = ({film, clickHandler, mouseOverHandler}: FilmCardProps): JSX.Element =>
-  (
+
+const FilmCard = ({film, clickHandler}: FilmCardProps): JSX.Element => {
+  const [video, setVideo] = useState(false);
+  const [isArticleHover, setArticleHover] = useState(false);
+
+  const mouseOverHandler = () => {
+    setArticleHover(true);
+  };
+
+  const mouseOutHandler = () => {
+    setArticleHover(false);
+    setVideo(false);
+  };
+
+  useEffect(() => {
+    const hoverTimout =
+      isArticleHover
+        ? setTimeout(() => {
+          setVideo(true);
+        }, 1000)
+        : '';
+
+    return () => {
+      clearTimeout(hoverTimout);
+    };
+  }, [isArticleHover]);
+
+  return (
     <article
       key={film.id}
       onClick={() => clickHandler(film)}
-      onMouseOver={() => mouseOverHandler(film)}
+      onMouseOver={mouseOverHandler}
+      onMouseOut={mouseOutHandler}
       className="small-film-card catalog__films-card"
     >
       <div className="small-film-card__image">
-        <img
-          src={film.posterImage}
-          alt={film.name}
-          width="280"
-          height="175"
-        />
+        {
+          video ? (
+            <VideoPlayer videoLink={film.videoLink} posterImage={film.posterImage}></VideoPlayer>
+          ) : (
+            <img
+              src={film.posterImage}
+              alt={film.name}
+              width="280"
+              height="175"
+            />
+          )
+        }
       </div>
-      <h3 className="small-film-card__title">
-        <Link
-          to={`${AppRoute.Film.replace(':id', film.id).replace(':info', FilmRoute.Overview)}`}
-          className="small-film-card__link"
-        >
-          {film.name}
-        </Link>
-      </h3>
+      {
+        !video && (
+          <h3 className="small-film-card__title">
+            <Link
+              to={`${AppRoute.Film.replace(':id', film.id).replace(':info', FilmRoute.Overview)}`}
+              className="small-film-card__link"
+            >
+              {film.name}
+            </Link>
+          </h3>
+        )
+      }
     </article>
   );
+};
 
 export default FilmCard;
