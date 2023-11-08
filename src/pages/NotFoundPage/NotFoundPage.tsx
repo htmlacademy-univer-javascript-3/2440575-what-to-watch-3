d6c33d5ccd1c9fe3a2fd7f2dbf685849ae14ec83
monikahuenika <91833875+monikahuenika@users.noreply.github.com>
2023-11-08 08:52:12 +0500

    add 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 const NotFound404 = () => (
-  <h1 style={{textAlign: 'center', color: 'red'}}>404 Not Found</h1>
+  <h1 style={{textAlign: 'center', color: 'red'}}>404 Not  Found</h1>
 );
 
 export default NotFound404;
